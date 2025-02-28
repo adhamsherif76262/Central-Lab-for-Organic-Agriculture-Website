7ff7a0f7723014a929cd283e8377852fa6a60e0c
Adham Sherif <adhamsherif7261@gmail.com>
2025-02-28 20:30:10 +0200

    initial commit

---
+++ JAVASCRIPT/Previous_Presidents.js
@@ -0,0 +1,232 @@
+var Title_H1 = document.getElementById('Title_H1');
+
+var Title_H1_Ar = `
+المديرين السابقين
+`;
+var Title_H1_En = `
+Previous Directors
+`;
+
+var Directors_Section = document.getElementById("Directors_Section");
+// var Directors_Section_Ar = document.getElementById("Directors_Section_Ar");
+// var Directors_Section_En = document.getElementById("Directors_Section_En");
+
+var Directors_Section_Ar = `
+     <section class="PD_Ar u-align-center u-clearfix u-grey-5 u-lightbox u-section-1" id="sec-b31b_Ar">
+         <div class="u-clearfix u-sheet u-sheet-1">
+             <div class="custom-expanded u-layout-horizontal u-list u-list-1">
+                 <div class="u-repeater u-repeater-1">
+                     <div class="5 u-align-center u-container-align-center u-container-style u-custom-item u-image u-image-round u-list-item u-repeater-item u-shading u-shape-rectangle u-image-5"
+                         data-image-width="893" data-image-height="1000" data-animation-name="customAnimationIn"
+                         data-animation-duration="1500" data-animation-delay="250">
+                         <div class="u-container-layout u-similar-container u-container-layout-5">
+                             <h3 class="u-text u-text-15"> أ.د. توفيق حافظ عبد المعطي <br>
+                             </h3>
+                             <h3 class="u-custom-font u-font-arial u-text u-text-palette-3-base u-text-16">
+                                 أستاذ المكافحة البيولوجية - معهد بحوث امراض النباتات</h3>
+                             <p class="u-custom-font u-font-oswald u-text u-text-body-alt-color u-text-17">٢٠٠٢ <span
+                                     class="u-file-icon u-icon u-text-white u-icon-5" data-animation-name=""
+                                     data-animation-duration="0" data-animation-delay="0"
+                                     data-animation-direction=""><img class="Arrow_Img" src="../Images/Directors/Filled-Right-Arrow.png"
+                                         alt=""></span>&nbsp;٢٠٠٤                                      
+                             </p>
+                         </div>
+                     </div>
+                     <div class="4 u-align-center u-container-align-center u-container-style u-custom-item u-image u-image-round u-list-item u-repeater-item u-shading u-shape-rectangle u-image-4"
+                         data-image-width="759" data-image-height="1000" data-animation-name="customAnimationIn"
+                         data-animation-duration="1500" data-animation-delay="250">
+                         <div class="u-container-layout u-similar-container u-container-layout-4">
+                             <h3 class="u-text u-text-12"> أ.د. بهجت السيد علي<br>
+                             </h3>
+                             <h3 class="u-custom-font u-font-arial u-text u-text-palette-3-base u-text-13">استاذ انتاج
+                                 الاسمدة العضوية - معهد بحوث الاراضي والمياه والبيئة</h3>
+                             <p class="u-custom-font u-font-oswald u-text u-text-body-alt-color u-text-14">٢٠٠٤ <span
+                                     class="u-file-icon u-icon u-text-white u-icon-4" data-animation-name=""
+                                     data-animation-duration="0" data-animation-delay="0"
+                                     data-animation-direction=""><img class="Arrow_Img" src="../Images/Directors/Filled-Right-Arrow.png"
+                                         alt=""></span>&nbsp;٢٠٠٦
+                             </p>
+                         </div>
+                     </div>
+                     <div class="3 u-align-center u-container-align-center u-container-style u-custom-item u-image u-image-round u-list-item u-repeater-item u-shading u-shape-rectangle u-image-3"
+                         data-image-width="858" data-image-height="1000" data-animation-name="customAnimationIn"
+                         data-animation-duration="1500" data-animation-delay="250">
+                         <div class="u-container-layout u-similar-container u-container-layout-3">
+                             <h3 class="u-text u-text-9">أ.د. عماد عبد القادر حسن<br>
+                             </h3>
+                             <h3 class="u-custom-font u-font-arial u-text u-text-palette-3-base u-text-10"> أستاذ الخضر -
+                                 معهد بحوث البساتين</h3>
+                             <p class="u-custom-font u-font-oswald u-text u-text-body-alt-color u-text-11">٢٠٠٦ <span
+                                     class="u-file-icon u-icon u-text-white u-icon-3" data-animation-name=""
+                                     data-animation-duration="0" data-animation-delay="0"
+                                     data-animation-direction=""><img class="Arrow_Img" src="../Images/Directors/Filled-Right-Arrow.png"
+                                         alt=""></span>&nbsp;٢٠١٢
+                             </p>
+                         </div>
+                     </div>
+                     <div class="2 u-align-center u-container-align-center u-container-style u-custom-item u-image u-image-round u-list-item u-repeater-item u-shading u-shape-rectangle u-image-2"
+                         data-image-width="797" data-image-height="1000" data-animation-name="customAnimationIn"
+                         data-animation-duration="1500" data-animation-delay="250">
+                         <div class="u-container-layout u-similar-container u-container-layout-2">
+                             <h3 class="u-text u-text-6">أ.د. عاطف عبد العزيز<br>
+                             </h3>
+                             <h3 class="u-custom-font u-font-arial u-text u-text-palette-3-base u-text-7">استاذ
+                                 الميكروبيولوجيا الزراعية - معهد بحوث الاراضي والمياه والبيئة</h3>
+                             <p class="u-custom-font u-font-oswald u-text u-text-body-alt-color u-text-8">٢٠١٢ <span
+                                     class="u-file-icon u-icon u-text-white u-icon-2" data-animation-name=""
+                                     data-animation-duration="0" data-animation-delay="0"
+                                     data-animation-direction=""><img class="Arrow_Img" src="../Images/Directors/Filled-Right-Arrow.png"
+                                         alt=""></span>&nbsp;٢٠١٩
+                             </p>
+                         </div>
+                     </div>
+                     <div class="1 u-align-center u-container-align-center u-container-style u-custom-item u-image u-image-round u-list-item u-repeater-item u-shading u-shape-rectangle u-image-1 tetss"
+                         data-image-width="755" data-image-height="1000" data-animation-name="customAnimationIn"
+                         data-animation-duration="1500" data-animation-delay="250">
+                         <div class="u-container-layout u-similar-container u-container-layout-1">
+                             <h3 class="u-text u-text-3">د. مايسة عبد المنعم لطفي<br>
+                             </h3>
+                             <h3 class="u-custom-font u-font-arial u-text u-text-palette-3-base u-text-4">أستاذ أمراض
+                                 النباتات - معهد بحوث امراض النباتات</h3>
+                             <p class="u-custom-font u-font-oswald u-text u-text-body-alt-color u-text-5">٢٠١٩ <span
+                                     class="u-file-icon u-icon u-text-white u-icon-1" data-animation-name=""
+                                     data-animation-duration="0" data-animation-delay="0"
+                                     data-animation-direction=""><img class="Arrow_Img" src="../Images/Directors/Filled-Right-Arrow.png"
+                                         alt=""></span>&nbsp;٢٠٢٤
+                             </p>
+                         </div>
+                     </div>
+                 </div>
+                ${Carousel_Arrows}
+             </div>
+         </div>
+     </section>
+ `;
+var Directors_Section_En = `
+      <section class="PD_En u-align-center u-clearfix u-grey-5 u-lightbox u-section-1" id="sec-b31b_En">
+         <div class="u-clearfix u-sheet u-sheet-1">
+             <div class="custom-expanded u-layout-horizontal u-list u-list-1">
+                 <div class="u-repeater u-repeater-1">
+                     <div class="5 u-align-center u-container-align-center u-container-style u-custom-item u-image u-image-round u-list-item u-repeater-item u-shading u-shape-rectangle u-image-5"
+                         data-image-width="893" data-image-height="1000" data-animation-name="customAnimationIn"
+                         data-animation-duration="1500" data-animation-delay="250">
+                         <div class="u-container-layout u-similar-container u-container-layout-5">
+                             <h3 class="u-text u-text-15"> Prof. Dr. Tawfik Hafez Abdel Motea<br>
+                             </h3>
+                             <h3 class="u-custom-font u-font-arial u-text u-text-palette-3-base u-text-16"> Professor of
+                                 Biological Control - Plant Pathology Research Institute</h3>
+                             <p class="u-custom-font u-font-oswald u-text u-text-body-alt-color u-text-17">2002 <span
+                                     class="u-file-icon u-icon u-text-white u-icon-5" data-animation-name=""
+                                     data-animation-duration="0" data-animation-delay="0"
+                                     data-animation-direction=""><img src="../Images/Directors/Filled-Right-Arrow.png"
+                                         alt=""></span>&nbsp;2004
+                             </p>
+                         </div>
+                     </div>
+                     <div class="4 u-align-center u-container-align-center u-container-style u-custom-item u-image u-image-round u-list-item u-repeater-item u-shading u-shape-rectangle u-image-4"
+                         data-image-width="759" data-image-height="1000" data-animation-name="customAnimationIn"
+                         data-animation-duration="1500" data-animation-delay="250">
+                         <div class="u-container-layout u-similar-container u-container-layout-4">
+                             <h3 class="u-text u-text-12"> Prof. Bahgat El-Sayed Ali<br>
+                             </h3>
+                             <h3 class="u-custom-font u-font-arial u-text u-text-palette-3-base u-text-13"> Professor of
+                                 Organic Fertilizer Production - Soil, Water, and Environment
+                                 Research Institute</h3>
+                             <p class="u-custom-font u-font-oswald u-text u-text-body-alt-color u-text-14">2004 <span
+                                     class="u-file-icon u-icon u-text-white u-icon-4" data-animation-name=""
+                                     data-animation-duration="0" data-animation-delay="0"
+                                     data-animation-direction=""><img src="../Images/Directors/Filled-Right-Arrow.png"
+                                         alt=""></span>&nbsp;2006
+                             </p>
+                         </div>
+                     </div>
+                     <div class="3 u-align-center u-container-align-center u-container-style u-custom-item u-image u-image-round u-list-item u-repeater-item u-shading u-shape-rectangle u-image-3"
+                         data-image-width="858" data-image-height="1000" data-animation-name="customAnimationIn"
+                         data-animation-duration="1500" data-animation-delay="250">
+                         <div class="u-container-layout u-similar-container u-container-layout-3">
+                             <h3 class="u-text u-text-9"> Prof. Emad Abdel Qader Hassan<br>
+                             </h3>
+                             <h3 class="u-custom-font u-font-arial u-text u-text-palette-3-base u-text-10"> Professor of
+                                 Vegetables - Horticulture Research Institute</h3>
+                             <p class="u-custom-font u-font-oswald u-text u-text-body-alt-color u-text-11">2006 <span
+                                     class="u-file-icon u-icon u-text-white u-icon-3" data-animation-name=""
+                                     data-animation-duration="0" data-animation-delay="0"
+                                     data-animation-direction=""><img src="../Images/Directors/Filled-Right-Arrow.png"
+                                         alt=""></span>&nbsp;2012
+                             </p>
+                         </div>
+                     </div>
+                     <div class="2 u-align-center u-container-align-center u-container-style u-custom-item u-image u-image-round u-list-item u-repeater-item u-shading u-shape-rectangle u-image-2"
+                         data-image-width="797" data-image-height="1000" data-animation-name="customAnimationIn"
+                         data-animation-duration="1500" data-animation-delay="250">
+                         <div class="u-container-layout u-similar-container u-container-layout-2">
+                             <h3 class="u-text u-text-6"> Prof. Dr. Atef Abdel Aziz<br>
+                             </h3>
+                             <h3 class="u-custom-font u-font-arial u-text u-text-palette-3-base u-text-7"> Professor of
+                                 Agricultural Microbiology - Soil, Water, and Environment Research
+                                 Institute</h3>
+                             <p class="u-custom-font u-font-oswald u-text u-text-body-alt-color u-text-8">2012 <span
+                                     class="u-file-icon u-icon u-text-white u-icon-2" data-animation-name=""
+                                     data-animation-duration="0" data-animation-delay="0"
+                                     data-animation-direction=""><img src="../Images/Directors/Filled-Right-Arrow.png"
+                                         alt=""></span>&nbsp;2019
+                             </p>
+                         </div>
+                     </div>
+                     <div class="1 u-align-center u-container-align-center u-container-style u-custom-item u-image u-image-round u-list-item u-repeater-item u-shading u-shape-rectangle u-image-1 tetss"
+                         data-image-width="755" data-image-height="1000" data-animation-name="customAnimationIn"
+                         data-animation-duration="1500" data-animation-delay="250">
+                         <div class="u-container-layout u-similar-container u-container-layout-1">
+                             <h3 class="u-text u-text-3"> Dr. Maysa Abdel Moneim <br>
+                             </h3>
+                             <h3 class="u-custom-font u-font-arial u-text u-text-palette-3-base u-text-4">Professor of
+                                 Plant Pathology - Plant Pathology Research Institute</h3>
+                             <p class="u-custom-font u-font-oswald u-text u-text-body-alt-color u-text-5">2019 <span
+                                     class="u-file-icon u-icon u-text-white u-icon-1" data-animation-name=""
+                                     data-animation-duration="0" data-animation-delay="0"
+                                     data-animation-direction=""><img src="../Images/Directors/Filled-Right-Arrow.png"
+                                         alt=""></span>&nbsp;2024
+                             </p>
+                         </div>
+                     </div>
+                 </div>
+                 ${Carousel_Arrows}
+             </div>
+         </div>
+     </section>
+`;
+
+function Previous_Presidents_Load_Content_Ar() {
+  jQuery("#waterdrop").raindrops({
+    color: "#1c1f2f",
+    canvasHeight: 150,
+    density: 0.1,
+    frequency: 10,
+  });
+  Title_H1.innerHTML = Title_H1_Ar;
+  Title_H1.style.direction = "rtl";
+//   Directors_Section_En.style.display="none";
+//   Directors_Section_Ar.style.display="block";
+  Directors_Section.innerHTML = Directors_Section_Ar;
+  
+}
+function Previous_Presidents_Load_Content_En() {
+  jQuery("#waterdrop").raindrops({
+    color: "#1c1f2f",
+    canvasHeight: 150,
+    density: 0.1,
+    frequency: 10,
+  });
+  Title_H1.innerHTML = Title_H1_En;
+  Title_H1.style.direction = "ltr";
+    // Directors_Section_Ar.style.display = "none";
+    // Directors_Section_En.style.display = "block";
+  Directors_Section.innerHTML = Directors_Section_En;
+  
+}
+if (localStorage.getItem("Permenant_Language") === "English") {
+  Previous_Presidents_Load_Content_En();
+}
+if (localStorage.getItem("Permenant_Language") === "عربي") {
+  Previous_Presidents_Load_Content_Ar();
+}
